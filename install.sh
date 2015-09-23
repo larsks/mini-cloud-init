8b1279de224edc81659976557ec86d74896c8b7f
Lars Kellogg-Stedman <lars@redhat.com>
2015-09-23 15:47:46 -0400

    init script must be executable

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 install -m 755 cloud-init /bin/cloud-init
-install -m 644 cloud-init.init /etc/init.d/cloud-init
+install -m 755 cloud-init.init /etc/init.d/cloud-init
 install -m 755 growpart /sbin/growpart
 install -m 644 growpart.files /etc/mkinitfs/features.d/growpart.files
